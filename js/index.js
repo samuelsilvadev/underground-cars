e99d8af1b08acb5b777c265cc15b3ea5064878de
Samuel SIlva <samuelsilvawb@gmail.com>
2018-05-10 15:56:44 -0300

    chore : create general rule to editors
    - created file editorconfig
    - improved all files to same pattern

--- js/index.js
+++ js/index.js
@@ -1,146 +1,111 @@
 (function (DOM, AJAX) {
-  'use strict';
+	'use strict';
 
-  /*
-  Vamos estruturar um pequeno app utilizando módulos.
-  Nosso APP vai ser um cadastro de carros. Vamos fazê-lo por partes.
-  A primeira etapa vai ser o cadastro de veículos, de deverá funcionar da
-  seguinte forma:
-  - No início do arquivo, deverá ter as informações da sua empresa - nome e
-  telefone (já vamos ver como isso vai ser feito)
-  - Ao abrir a tela, ainda não teremos carros cadastrados. Então deverá ter
-  um formulário para cadastro do carro, com os seguintes campos:
-    - Imagem do carro (deverá aceitar uma URL)
-    - Marca / Modelo
-    - Ano
-    - Placa
-    - Cor
-    - e um botão "Cadastrar"
+	function appModule() {
 
-  Logo abaixo do formulário, deverá ter uma tabela que irá mostrar todos os
-  carros cadastrados. Ao clicar no botão de cadastrar, o novo carro deverá
-  aparecer no final da tabela.
+		var $carImage = new DOM('[data-js="car-input-image"]');
+		var $carBrand = new DOM('[data-js="car-input-brand"]');
+		var $carYear = new DOM('[data-js="car-input-year"]');
+		var $carPlate = new DOM('[data-js="car-input-plate"]');
+		var $carColor = new DOM('[data-js="car-input-color"]');
+		var $btnCarForm = new DOM('[data-js="btn-car-form"]');
+		var $tbodyListCars = new DOM('[data-js="car-list-body"]');
+		var $companyName = new DOM('[data-js="company-name"]');
+		var $companyTelephone = new DOM('[data-js="company-telephone"]');
 
-  Agora você precisa dar um nome para o seu app. Imagine que ele seja uma
-  empresa que vende carros. Esse nosso app será só um catálogo, por enquanto.
-  Dê um nome para a empresa e um telefone fictício, preechendo essas informações
-  no arquivo company.json que já está criado.
+		var savedCars = [];
 
-  Essas informações devem ser adicionadas no HTML via Ajax.
+		function init() {
+			getCompanyData();
+			$btnCarForm.on('click', handleSubmitCarForm);
+		}
 
-  Parte técnica:
-  Separe o nosso módulo de DOM criado nas últimas aulas em
-  um arquivo DOM.js.
+		function getCompanyData() {
+			AJAX.get('./company.json', function (data) {
+				if (!data.err) {
+					$companyName.getFirst().textContent = data.name;
+					$companyTelephone.getFirst().textContent = data.phone;
+				}
+			});
+		}
 
-  E aqui nesse arquivo, faça a lógica para cadastrar os carros, em um módulo
-  que será nomeado de "app".
-  */
+		function saveCar() {
 
-  function appModule() {
+			var newCarObj = {
+				image: $carImage.getFirst().value,
+				brand: $carBrand.getFirst().value,
+				year: $carYear.getFirst().value,
+				plate: $carPlate.getFirst().value,
+				color: $carColor.getFirst().value
+			};
 
-    var $carImage = new DOM('[data-js="car-input-image"]');
-    var $carBrand = new DOM('[data-js="car-input-brand"]');
-    var $carYear = new DOM('[data-js="car-input-year"]');
-    var $carPlate = new DOM('[data-js="car-input-plate"]');
-    var $carColor = new DOM('[data-js="car-input-color"]');
-    var $btnCarForm = new DOM('[data-js="btn-car-form"]');
-    var $tbodyListCars = new DOM('[data-js="car-list-body"]');
-    var $companyName = new DOM('[data-js="company-name"]');
-    var $companyTelephone = new DOM('[data-js="company-telephone"]');
+			if (_isFormValid(newCarObj)) {
+				savedCars.push(newCarObj);
+				updateTableCar(newCarObj);
+				clearForm();
+			}
+		}
 
-    var savedCars = [];
+		function clearForm() {
+			$carImage.getFirst().value = '';
+			$carBrand.getFirst().value = '';
+			$carYear.getFirst().value = '';
+			$carPlate.getFirst().value = '';
+			$carColor.getFirst().value = '';
+		}
 
-    function init() {
-      getCompanyData();
-      $btnCarForm.on('click', handleSubmitCarForm);
-    }
+		function _isFormValid(objCar) {
 
-    function getCompanyData() {
-      AJAX.get('./company.json', function (data) {
-        if (!data.err) {
-          $companyName.getFirst().textContent = data.name;
-          $companyTelephone.getFirst().textContent = data.phone;
-        }
-      });
-    }
+			var formIsValid = true;
+			for (var prop in objCar) {
+				if (objCar.hasOwnProperty(prop)) {
+					if (!objCar[prop]) {
+						formIsValid = false;
+					}
+				}
+			}
+			return formIsValid;
+		}
 
-    function saveCar() {
+		function updateTableCar(obj) {
 
-      var newCarObj = {
-        image: $carImage.getFirst().value,
-        brand: $carBrand.getFirst().value,
-        year: $carYear.getFirst().value,
-        plate: $carPlate.getFirst().value,
-        color: $carColor.getFirst().value
-      };
+			var $fragment = document.createDocumentFragment();
+			var $newTr = document.createElement('tr');
 
-      if (_isFormValid(newCarObj)) {
-        savedCars.push(newCarObj);
-        updateTableCar(newCarObj);
-        clearForm();
-      }
-    }
+			$newTr.appendChild(_createImg(obj.image));
+			$newTr.appendChild(_createTd(obj.brand));
+			$newTr.appendChild(_createTd(obj.year));
+			$newTr.appendChild(_createTd(obj.plate));
+			$newTr.appendChild(_createTd(obj.color));
 
-    function clearForm() {
-      $carImage.getFirst().value = '';
-      $carBrand.getFirst().value = '';
-      $carYear.getFirst().value = '';
-      $carPlate.getFirst().value = '';
-      $carColor.getFirst().value = '';
-    }
+			$fragment.appendChild($newTr)
 
-    function _isFormValid(objCar) {
+			$tbodyListCars.getFirst().appendChild($fragment);
+		}
 
-      var formIsValid = true;
-      for (var prop in objCar) {
-        if (objCar.hasOwnProperty(prop)) {
-          if (!objCar[prop]) {
-            formIsValid = false;
-          }
-        }
-      }
-      return formIsValid;
-    }
+		function _createTd(text) {
+			var $newTd1 = document.createElement('td');
+			$newTd1.textContent = text;
+			return $newTd1;
+		}
 
-    function updateTableCar(obj) {
+		function _createImg(url) {
+			var $td = _createTd('');
+			var $newImg = document.createElement('img');
+			$newImg.src = url;
+			$td.appendChild($newImg)
+			return $td;
+		}
 
-      var $fragment = document.createDocumentFragment();
-      var $newTr = document.createElement('tr');
+		function handleSubmitCarForm(e) {
+			e.preventDefault();
+			saveCar();
+		}
 
-      $newTr.appendChild(_createImg(obj.image));
-      $newTr.appendChild(_createTd(obj.brand));
-      $newTr.appendChild(_createTd(obj.year));
-      $newTr.appendChild(_createTd(obj.plate));
-      $newTr.appendChild(_createTd(obj.color));
+		return {
+			init: init
+		};
+	}
 
-      $fragment.appendChild($newTr)
-
-      $tbodyListCars.getFirst().appendChild($fragment);
-    }
-
-    function _createTd(text) {
-      var $newTd1 = document.createElement('td');
-      $newTd1.textContent = text;
-      return $newTd1;
-    }
-
-    function _createImg(url) {
-      var $td = _createTd('');
-      var $newImg = document.createElement('img');
-      $newImg.src = url;
-      $td.appendChild($newImg)
-      return $td;
-    }
-
-    function handleSubmitCarForm(e) {
-      e.preventDefault();
-      saveCar();
-    }
-
-    return {
-      init: init
-    };
-  }
-
-  appModule().init();
+	appModule().init();
 })(window.DOM, window.AJAX);
