c544bb9436016dfc6cbe3bb03420ead6f771134e
Samuel SIlva <samuelsilvawb@gmail.com>
2018-05-10 15:56:44 -0300

    chore : create general rule to editors
    - created file editorconfig
    - improved all files to same pattern

--- js/libs/ajax.js
+++ js/libs/ajax.js
@@ -1,34 +1,34 @@
 (function (window, document) {
-  'use strict';
+	'use strict';
 
 
-  function AJAX() {
+	function AJAX() {
 
-    return {
-      get: function get(url, callback) {
-        var ajax = new XMLHttpRequest();
-        ajax.open('GET', url, true);
-        ajax.send();
+		return {
+			get: function get(url, callback) {
+				var ajax = new XMLHttpRequest();
+				ajax.open('GET', url, true);
+				ajax.send();
 
-        ajax.addEventListener('readystatechange', function () {
-          if (ajax.readyState === 4 && ajax.status === 200) {
-            try {
-              var data = JSON.parse(ajax.responseText);
-              callback(data);
-            } catch (err) {
-              console.error(err);
-              callback({ err: true, msg: err, status: ajax.status });
-            }
-          }
+				ajax.addEventListener('readystatechange', function () {
+					if (ajax.readyState === 4 && ajax.status === 200) {
+						try {
+							var data = JSON.parse(ajax.responseText);
+							callback(data);
+						} catch (err) {
+							console.error(err);
+							callback({ err: true, msg: err, status: ajax.status });
+						}
+					}
 
-          if (ajax.readyState === 4 && ajax.status !== 200) {
-            callback({ err: true, msg: '', status: ajax.status });
-          }
-        }, false);
-      }
-    };
-  }
+					if (ajax.readyState === 4 && ajax.status !== 200) {
+						callback({ err: true, msg: '', status: ajax.status });
+					}
+				}, false);
+			}
+		};
+	}
 
-  window.AJAX = AJAX();
+	window.AJAX = AJAX();
 
 })(window, document);
